593253f85f38b45b2d8a024a3b3b22fc214ae93c
rafiuzzamansajib <mdrafiuzzamansajib@gmail.com>
2021-10-30 21:06:29 +0600

    Done project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import './Header.css'
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -13,23 +14,23 @@ const Header = () => {
                 <Navbar.Brand href="#home"><span>T</span>RAVEL</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Nav.Link className="header-link text-dark" as={HashLink} to="/home#home">Home</Nav.Link>
-                    <Nav.Link className="header-link text-dark" as={HashLink} to="/home#package">Package</Nav.Link>
-                    <Nav.Link className="header-link text-dark" as={HashLink} to="/home#reviwe">Reviwe</Nav.Link>
-                    <Nav.Link className="header-link text-dark" as={HashLink} to="/home#gallary">Gallary</Nav.Link>
+                    <Nav.Link className="header-link text-dark nav-bar" as={HashLink} to="/home#home">Home</Nav.Link>
+                    <Nav.Link className="header-link text-dark nav-bar" as={HashLink} to="/home#package">Package</Nav.Link>
+                    <Nav.Link className="header-link text-dark nav-bar" as={HashLink} to="/home#reviwe">Reviwe</Nav.Link>
+                    <Nav.Link className="header-link text-dark nav-bar" as={HashLink} to="/home#gallary">Gallary</Nav.Link>
                         {
                             user?.email?
-                            <Nav.Link className="header-link text-dark" as={Link} to="/addpackage">Add Package</Nav.Link>:
+                            <Nav.Link className="header-link text-dark nav-bar" as={Link} to="/addpackage">Add Package</Nav.Link>:
                             ""
                         }
                         {
                             user?.email?
-                            <Nav.Link className="header-link text-dark" as={Link} to="/managePacages">Manage Package</Nav.Link>:
+                            <Nav.Link className="header-link text-dark nav-bar" as={Link} to="/managePacages">Manage Package</Nav.Link>:
                             ""
                         }
                         {user?.email ?
                         <Button onClick={logOut} variant="light">Logout</Button> :
-                        <Nav.Link as={Link} className="header-link text-dark" to="/login">Login</Nav.Link>}
+                        <Nav.Link as={Link} className="header-link text-dark nav-bar" to="/login">Login</Nav.Link>}
                     <Navbar.Text>
                     <a href="#login">{user?.displayName}</a>
                     </Navbar.Text>
